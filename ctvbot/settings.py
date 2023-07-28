39763715078937481d2b98d61dfd415527864705
scarletoath <dragonlss@hotmail.com>
2023-07-28 15:10:49 -0500

    Added Settings.save_settings().
    Renamed Settings._generalSettings to _config, and changed it cache the configParser itself.
    - Updated General property to dynamically retrieve the General section from _config.

--- ctvbot/settings.py
+++ ctvbot/settings.py
@@ -7,15 +7,18 @@ logger = logging.getLogger(__name__)
 
 class Settings:
     def __init__(self, settings_file_name="settings.ini"):
-        self._generalSettings = {}
+        self._config = configparser.ConfigParser()
         self.pathed_file_name = os.path.join(os.getcwd(), "config", settings_file_name)
         self.load_settings()
 
     @property
     def General(self):
-        return self._generalSettings
+        return self._config['General']
 
     def load_settings(self):
-        config = configparser.ConfigParser()
+        config = self._config
         config.read(self.pathed_file_name)
-        self._generalSettings = config['General']
+
+    def save_settings(self):
+        with open(self.pathed_file_name, 'w') as configFile:
+            self._config.write(configFile)

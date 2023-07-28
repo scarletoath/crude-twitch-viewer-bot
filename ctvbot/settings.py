dfd43f9fc36d38bed0830f790d0e587d9e05706f
scarletoath <dragonlss@hotmail.com>
2023-07-28 14:53:30 -0500

    Fixed incorrect usage of configparser

--- ctvbot/settings.py
+++ ctvbot/settings.py
@@ -7,15 +7,15 @@ logger = logging.getLogger(__name__)
 
 class Settings:
     def __init__(self, settings_file_name="settings.ini"):
-        self._settings = {}
+        self._generalSettings = {}
         self.pathed_file_name = os.path.join(os.getcwd(), "config", settings_file_name)
         self.load_settings()
 
     @property
     def General(self):
-        return self._settings['General']
+        return self._generalSettings
 
     def load_settings(self):
         config = configparser.ConfigParser()
         config.read(self.pathed_file_name)
-        self._settings = config.sections()
+        self._generalSettings = config['General']
